32aff528672c63d62057bef48b309dc0058ddf79
abevers <arnoud.bevers@accenture.com>
2020-09-03 13:17:30 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: abevers <arnoud.bevers@accenture.com>

--- platforms/corda-ent/charts/idman/files/run.sh
+++ platforms/corda-ent/charts/idman/files/run.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # main run
-if [ -f {{ .Values.jarPath }}/identitymanager.jar ]
+if [ -f {{ .Values.config.volume.baseDir }}/identitymanager.jar ]
 then
-    sha256sum {{ .Values.jarPath }}/identitymanager.jar 
+    sha256sum {{ .Values.config.volume.baseDir }}/identitymanager.jar 
     cat etc/idman.conf
     echo
     echo "CENM: starting Identity Manager process ..."
     echo
-    java -Xmx{{ .Values.cordaJarMx }}G -jar {{ .Values.jarPath }}/identitymanager.jar -f {{ .Values.configPath }}/idman.conf
+    java -Xmx{{ .Values.cordaJar.memorySize }}{{ .Values.cordaJar.unit }} -jar {{ .Values.config.volume.baseDir }}/identitymanager.jar -f {{ .Values.config.configPath }}/idman.conf
     EXIT_CODE=${?}
 else
-    echo "Missing Identity Manager jar file in {{ .Values.jarPath }} folder:"
-    ls -al {{ .Values.jarPath }}
+    echo "Missing Identity Manager jar file in {{ .Valuesconfig.volume.baseDir }} folder:"
+    ls -al {{ .Values.config.volume.baseDir }}
     EXIT_CODE=110
 fi
 
 if [ "${EXIT_CODE}" -ne "0" ]
 then
-    HOW_LONG={{ .Values.sleepTimeAfterError }}
+    HOW_LONG={{ .Values.config.sleepTimeAfterError }}
     echo
     echo "exit code: ${EXIT_CODE} (error)"
     echo "Going to sleep for requested ${HOW_LONG} seconds to let you login and investigate."
